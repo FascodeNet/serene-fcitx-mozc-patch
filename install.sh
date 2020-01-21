a552e5159436d748f42e779683e579dac1248966
YamaD <yamad.linuxer@gmail.com>
2020-01-21 20:48:37 +0900

    fixed bug

--- install.sh
+++ install.sh
@@ -1,83 +1,90 @@
 #!/bin/bash
 
-EDIR=$PWD
+# Are u superuser?
+if [ ! $UID -eq 0 ]; then
+	echo "Permisson denied."
+	exit 1
+fi
 
-# check i'm in gui or cli
-if [[ -z $DISPLAY ]]; then
+# extracted directory
+EDIR=$(cd $(dirname $0); pwd)
+
+# real username
+USERNAME=${SUDO_USER:-$USER}
+
+# check env to install
+if [ -z $DISPLAY ]; then
 	# NO DISPLAY FOUND !!!! 
 	echo "You must run this script only in GUI !"
 	exit 1
+elif [ ! $USERNAME == $USER ]; then
+	if [ ! -d /home/$USERNAME ]; then
+		echo "Couldn't find homedirectory."
+		exit 1
+	fi
+	HOMEDIR=/home/$USERNAME
+else
+	HOMEDIR=/root
 fi
 
-# path to ExtractedDirectory
-EDIR=~/serene-fcitx-mozc-patch
-if [[ -z $EDIR/config/config ]]; then
-	echo "No configration file found. Please configration fcitx by yourself."
-	exit 1
+# relaunch fcitx
+if pidof fcitx >/dev/null 2>&1; then
+	fcitx -r >/dev/null 2>&1 &
+else
+	fcitx >/dev/null 2>&1 &
 fi
 
-fcitx -r > /dev/null 2>&1 &
-
-# check the existence of fcitx config dir and put config to it
-if [[ ! -e ~/.config/fcitx/config ]]; then
-
-cat << EOS
------------------------------------------
-!!!! PLEASE RELAUNCH THE INSTALLER !!!! 
------------------------------------------
-EOS
-
-	exec fcitx > /dev/null 2>&1 && \
-	exit 2
-fi
 
 # keyboard layout jp or us
 echo "Keymap to use (jp106):1 (US):2 Default=1 : "
 read ANSWER
 
 case $ANSWER in
-	"1" ) CHOSELANG=jp && echo "Keymap is jp106";;
-	"2" ) CHOSELANG=us && echo "Keymap is US";;
-	"" ) CHOSELANG=jp && echo "Keymap is jp106";;
+	"1" ) CHOSEMAP=jp && echo "Keymap is jp106";;
+	"2" ) CHOSEMAP=us && echo "Keymap is US";;
+	"" ) CHOSEMAP=jp && echo "Keymap is jp106";;
 	* ) echo "error" && exit 1;;
 esac
 
-# Ask root password.
-printf "Please enter your password. (The password is used to create setting files.)"
-read -sp root_password
 
-# installing message
-echo "Placing custom settings to ~/.config/fcitx/ now..."
-cp -rb $EDIR/config/config-$CHOSELANG ~/.config/fcitx/config && \
-cp -rb $EDIR/profile/profile-$CHOSELANG ~/.config/fcitx/profile && \
-cp -rb $EDIR/conf ~/.config/fcitx/ && \
-echo $root_password | sudo -s cp -rb $EDIR/config/config-$CHOSELANG /etc/skel/.config/fcitx/config && \
-echo $root_password | sudo -s cp -rb $EDIR/profile/profile-$CHOSELANG /etc/skel/.config/fcitx/profile && \
-echo $root_password | sudo -s cp -rb $EDIR/conf /etc/skel/.config/fcitx/ && \
+echo "Placing custom settings..."
+# local change
+cp -rb $EDIR/config/config-$CHOSEMAP $HOMEDIR/.config/fcitx/config && \
+cp -rb $EDIR/profile/profile-$CHOSEMAP $HOMEDIR/.config/fcitx/profile && \
+cp -rb $EDIR/conf $HOMEDIR/.config/fcitx/ && \
+# apply for systemwide
+cp -rb $EDIR/config/config-$CHOSEMAP /etc/skel/.config/fcitx/config && \
+cp -rb $EDIR/profile/profile-$CHOSEMAP /etc/skel/.config/fcitx/profile && \
+cp -rb $EDIR/conf /etc/skel/.config/fcitx/ && \
 
-# add config to xprofile for other linux distro
-#cat ~/.xprofile | grep "GTK_IM_MODULE=fcitx" > /dev/null
-if [[ -f /etc/os-release ]]; then
-	source /etc/os-release
-else
-	echo "/ etc / os-release does not exist." >&2
-	ID_LIKE=other
+
+# edit xprofile
+if [ -f $HOMEDIR/.xprofile ]; then
+	sed -i '/GTK_IM_MODULE/s/^/#/g' $HOMEDIR/.xprofile
+	sed -i '/QT_IM_MODULE/s/^/#/g' $HOMEDIR/.xprofile
+	sed -i '/XMODIFIERS=@im/s/^/#/g' $HOMEDIR/.xprofile
+fi
+# edit systemwide
+if [ -f /etc/skel/.xprofile ]; then
+	sed -i '/GTK_IM_MODULE=/s/^/#/g' /etc/skel/.xprofile
+	sed -i '/QT_IM_MODULE=/s/^/#/g' /etc/skel/.xprofile
+	sed -i '/XMODIFIERS=@im=/s/^/#/g' /etc/skel/.xprofile
 fi
 
-if [[ $? = 0 -a ! $ID_LIKE = "debian" ]]; then
-	echo GTK_IM_MODULE=fcitx >> ~/.xprofile
-	echo QT_IM_MODULE=fcitx >> ~/.xprofile
-	echo XMODIFIERS=@im=fcitx >> ~/.xprofile
-	echo $root_password | sudo -s echo GTK_IM_MODULE=fcitx >> ~/.xprofile
-	echo $root_password | sudo -s echo QT_IM_MODULE=fcitx >> ~/.xprofile
-	echo $root_password | sudo -s echo XMODIFIERS=@im=fcitx >> ~/.xprofile
-fi
+# local change
+echo GTK_IM_MODULE=fcitx >> $HOMEDIR/.xprofile
+echo QT_IM_MODULE=fcitx >> $HOMEDIR/.xprofile
+echo XMODIFIERS=@im=fcitx >> $HOMEDIR/.xprofile
+# systemwide
+echo GTK_IM_MODULE=fcitx >> /etc/skel/.xprofile
+echo QT_IM_MODULE=fcitx >> /etc/skel/.xprofile
+echo XMODIFIERS=@im=fcitx >> /etc/skel/.xprofile
 
+fcitx -r >/dev/null 2>&1 && \
 cat << EOS
 --------------------------------------
 INSTALL CONFIGS IS SUCCESSFULLY !!!! 
 --------------------------------------
 EOS
-exec fcitx -r > /dev/null 2>&1 && \
 exit 0
 
